38f68bccc8e124bfe82c9cdda8a350dd161395fc
허민엽 <hmya5789@gmail.com>
2025-11-25 04:46:53 +0900

    nyang으로 확장자 변경

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from core.interpreter import Interpreter
 interpreter = Interpreter()
 interpreter.reset()
 
-with open('./examples/simple.nya', encoding='utf-8') as f:
+with open('./examples/simple.nyang', encoding='utf-8') as f:
     for line in f:
         interpreter.exec_line(line)
