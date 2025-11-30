235566d226200260c05a87e9d2ac3ef872d1e5b1
허민엽 <hmya5789@gmail.com>
2025-11-30 13:48:10 +0900

    lexer 파싱 구조 변경

--- src/nyang/lexer.py
+++ src/nyang/lexer.py
@@ -10,11 +10,12 @@ from typing import List, Optional
 
 class TokenType(Enum):
     NYANG = auto()      # "냥" 연속 개수
-    NYA = auto()        # "냐" 연속 개수 (연산용)
+    NYA = auto()        # "냐" 연속 개수 (연산)
     INT = auto()        # . , 조합 -> 정수 리터럴 값
     TILDE = auto()      # "~"
     BANG = auto()       # "!", "!!"
     QUESTION = auto()   # "?" 
+    COMMENT = auto()    # "#"
     EOL = auto()        # 줄 끝
 
 
@@ -24,9 +25,8 @@ class Token:
     type: TokenType
     value: Optional[int] = None
 
+
 ALLOWED_CHARS = {"냥", "냐", ".", ",", "~", "!", "?"}
-
-
 
 def _strip_comments(line: str) -> str:
     """
@@ -37,56 +37,82 @@ def _strip_comments(line: str) -> str:
     return "".join(ch for ch in line if ch in ALLOWED_CHARS)
 
 
+def classify_char(ch: str) -> TokenType | None:
+    if ch == "냥":
+        return TokenType.NYANG
+    if ch == "냐":
+        return TokenType.NYA
+    if ch == "." or ch == ",":
+        return TokenType.INT
+    if ch == "~":
+        return TokenType.TILDE
+    if ch == "!":
+        return TokenType.BANG
+    if ch == "?":
+        return TokenType.QUESTION
+    # if ch == "#":
+    #     return TokenType.COMMENT
+    return None
+
+
 
 def lex_line(line: str) -> List[Token]:
     """
-    #### input: 명령어 라인
+    input: 명령어 라인
     - 한 줄을 토큰 리스트로 변환
-    #### output: 명령어에 대한 토큰 리스트
+    output: 명령어에 대한 토큰 리스트
     """
     line = _strip_comments(line)
+    # print(line)
     tokens: List[Token] = []
+    current_type: TokenType | None = None
+    count: int = 0
 
     if not line:
-        return tokens
-    
-    
-    bodys = {"냥", "냐"}
-    symbols = {"~", "!", "?"}
-    numbers = {".", ","}
+        return []
 
-    # 줄 끝 기호 파악
-    end_char = line[-1] if line[-1] in {"~", "!", "?"} else None # ; print(f'end_char = {end_char}')
-    body = line[:-1] if end_char else line # ; print(f'body = {body}')
+    for ch in line:
+        ttype = classify_char(ch)
 
-    # 1) 앞부분: NYANG / NYA / INT 처리
-    nyang_count = body.count("냥") # ; print(f'nyang_count = {nyang_count}')
-    nya_count = body.count("냐") # ; print(f'nya_count = {nya_count}')
-    dot_count = body.count(".") # ; print(f'dot_count = {dot_count}')
-    comma_count = body.count(",") # ; print(f'comma_count = {comma_count}')
+        if ttype is None:
+            if (current_type is not None) and (count):
+                tokens.append(Token(current_type, count))
+                current_type = None
+                count = 0
+            continue
 
-    # 
-    if nyang_count > 0:
-        tokens.append(Token(TokenType.NYANG, nyang_count))
-    if nya_count > 0:
-        tokens.append(Token(TokenType.NYA, nya_count))
-    if dot_count + comma_count > 0:
-        value = dot_count - comma_count
-        tokens.append(Token(TokenType.INT, value))
+        if (current_type is None) or (current_type != ttype):
+            if current_type is not None and count:
+                tokens.append(Token(current_type, count))
 
-    # 2) 끝 기호 처리
-    if end_char == "~":
-        tokens.append(Token(TokenType.TILDE))
-    elif end_char == "?":
-        tokens.append(Token(TokenType.QUESTION))
-    elif end_char == "!":
-        bang_count = line.count("!")
-        tokens.append(Token(TokenType.BANG, bang_count))
-    # elif end_char == "\n":
-    #     tokens.append(Token(TokenType.EOL))
+            current_type = ttype
+
+            if ttype == TokenType.INT:
+                count = 1 if ch == '.' else -1
+            else:
+                count = 1
+        else:
+            if ttype == TokenType.INT:
+                count += 1 if ch == '.' else -1
+            else:
+                count += 1
+
+    # 마지막 토큰 flush
+    if current_type is not None and count:
+        tokens.append(Token(current_type, count))
 
     return tokens
 
+
 # 테스트
 if __name__ == "__main__":
-    print(lex_line("....!")) 
+    print(lex_line("냐!!"))
+    # [토큰화 방법]
+    # 1. (NYANG,2) (INT, 3) (BANG, 2) (INT, 4) (BANG, 1)
+    # 2. (NYANG, 2) (INT, 7), (BANG, 3)
+
+    # 냥냥...!..,,,..냥냥...!!,,,~
+    # 1. (NYANG, 4) (DOT, 10), (BANG, 3), (COMMA, 6), (TILDE, 1)
+    # 2. (NYANG, 2) (DOT, 3), (BANG, 1), (DOT, 2), (COMMA, 3), (DOT, 2), (NYANG, 2), (DOT, 3), (BANG, 2), (COMMA, 3), (TILDE, 1)
+    # 3. (NYANG, 2) (INT, 3) (BANG, 1) (INT, 1) (BANG, 2) (INT, -3) (TILDE, 1)
+    # 내가 원하는건 3번 방법~ 
