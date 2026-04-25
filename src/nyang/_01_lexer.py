77c628be6379f7076736d079f07c046e7235415e
허민엽 <hmya5789@gmail.com>
2026-04-25 13:27:47 +0900

    lex_line 함수 위치 변경

--- src/nyang/_01_lexer.py
+++ src/nyang/_01_lexer.py
@@ -47,67 +47,6 @@ def classify_char(ch: str) -> TokenType | None:
     return None
 
 
-
-# def lex_line(line: str) -> List[Token]:
-#     line = _strip_comments(line)
-#     tokens: List[Token] = []
-#     current_type: TokenType | None = None
-#     count: int = 0
-#     n = len(line)
-
-#     if not line.strip(): return []
-
-#     for i in range(n):
-#         ch = line[i]
-#         if ch.isspace():
-#             if current_type is not None:
-#                 tokens.append(Token(current_type, count))
-#                 current_type, count = None, 0
-#             continue
-
-#         if ch == '야' and line[i+1] == '옹':
-#             tokens.append(Token(TokenType.YAONG))
-#             i += 1
-#             continue
-
-#         ttype = classify_char(ch)
-#         if ttype is None:
-#             raise SyntaxError(f"어휘 오류: '{ch}'는 허용하지 않습니다.")
-
-#         # 1. 타입이 바뀌면 지금까지 쌓인 토큰을 저장(Flush)
-#         if current_type != ttype:
-#             if current_type is not None:
-#                 tokens.append(Token(current_type, count))
-            
-#             # 새 토큰 시작
-#             current_type = ttype
-#             if ttype == TokenType.INT:
-#                 count = 1 if ch == '.' else -1
-#             else:
-#                 count = 1
-        
-#         # 2. 같은 타입이면 계속 개수만 세기 (제한 없이!)
-#         else:
-#             if ttype == TokenType.INT:
-#                 count += 1 if ch == "." else -1
-#             else:
-#                 count += 1
-
-#     # 마지막 남은 토큰 처리
-#     if current_type is not None:
-#         tokens.append(Token(current_type, count))
-
-#     return tokens
-
-
-def print_lex_line(line: List[Token]) -> None:
-    for token in line:
-        print(f"({token.type.name},{token.value})", end=' ')
-
-
-
-
-
 def lex_line(line: str) -> List[Token]:
     line = _strip_comments(line)
     tokens: List[Token] = []
@@ -165,3 +104,8 @@ def lex_line(line: str) -> List[Token]:
         tokens.append(Token(current_type, count))
 
     return tokens
+
+
+def print_lex_line(line: List[Token]) -> None:
+    for token in line:
+        print(f"({token.type.name},{token.value})", end=' ')
